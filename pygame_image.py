b5e335e4735973c1807b7f9fcd2d78799a664ef3
c0a250169c <c0a250169c@edu.teu.ac.jp>
2026-06-09 16:55:55 +0900

    こうかとん同じ速度で左

--- pygame_image.py
+++ pygame_image.py
@@ -9,20 +9,49 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img,True, False)
+
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)  # 左右反転
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (300, 200)
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+
+        mx=-1
+        my=0
+
+        #if key_lst[pg.K_UP]:
+         #   my-=1#上に進む
+        #if key_lst[pg.K_DOWN]:
+         #   my+=1#下に進む
+        #if key_lst[pg.K_LEFT]:
+         #   mx-=1#左に進む
+        #if key_lst[pg.K_RIGHT]:
+         #   mx+=2#右に進む
+        #kk_rct.move_ip(mx, my) 
+
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)#上に進む
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,+1)#下に進む
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(-1,0)#左に進む
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(+1,0)#右に進む
         x = tmr%3200
         screen.blit(bg_img, [-x, 0]) #5
         screen.blit(bg_img2, [-x+1600, 0]) #7
         screen.blit(bg_img, [-x+3200, 0]) #9
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
